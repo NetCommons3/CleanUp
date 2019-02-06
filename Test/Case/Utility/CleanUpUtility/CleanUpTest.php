12397a1d096c3295ed3a02ad9b8fbf5810412207
Mitsuru Mutaguchi <akagane99@gmail.com>
2019-02-06 17:40:33 +0900

    test: Fixtures指定の見直し

--- Test/Case/Utility/CleanUpUtility/CleanUpTest.php
+++ Test/Case/Utility/CleanUpUtility/CleanUpTest.php
@@ -20,6 +20,55 @@ App::uses('CleanUpUtility', 'CleanUp.Utility');
 class CleanUpUtilityCleanUpUtilityCleanUpTest extends CleanUpCakeTestCase {
 
 /**
+ * Fixtures
+ *
+ * @var array
+ * @see NetCommonsCakeTestCase::$_defaultFixtures よりコピー
+ */
+	protected $_defaultFixtures = array(
+		'plugin.blocks.block',
+		'plugin.blocks.block_role_permission',
+		'plugin.blocks.block_setting',
+		'plugin.blocks.blocks_language',
+		'plugin.boxes.box',
+		'plugin.boxes.boxes_page_container',
+		'plugin.data_types.data_type',
+		'plugin.data_types.data_type_choice',
+		'plugin.files.upload_file',
+		'plugin.files.upload_files_content',
+		'plugin.frames.frame',
+		'plugin.frames.frame_public_language',
+		'plugin.frames.frames_language',
+		'plugin.m17n.language',
+		'plugin.mails.mail_queue',
+		'plugin.mails.mail_queue_user',
+		'plugin.mails.mail_setting',
+		'plugin.pages.page',
+		'plugin.pages.page_container',
+		'plugin.plugin_manager.plugin',
+		//'plugin.plugin_manager.plugins_role',
+		//'plugin.roles.default_role_permission',
+		'plugin.roles.role',
+		'plugin.rooms.roles_room',
+		'plugin.rooms.roles_rooms_user',
+		'plugin.rooms.room',
+		'plugin.rooms.rooms_language',
+		//'plugin.rooms.room_role',
+		//'plugin.rooms.room_role_permission',
+		'plugin.rooms.space',
+		'plugin.site_manager.site_setting',
+		'plugin.topics.topic',
+		'plugin.topics.topic_readable',
+		'plugin.topics.topic_user_status',
+		'plugin.user_attributes.user_attribute',
+		'plugin.user_attributes.user_attribute_choice',
+		'plugin.user_attributes.user_attribute_setting',
+		'plugin.user_roles.user_attributes_role',
+		'plugin.users.user',
+		'plugin.users.users_language',
+	);
+
+/**
  * Plugin name
  *
  * @var string
