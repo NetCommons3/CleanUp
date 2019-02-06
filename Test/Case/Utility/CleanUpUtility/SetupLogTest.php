59a67e0fd69597c85861cd1722900cad86280c96
Mitsuru Mutaguchi <akagane99@gmail.com>
2019-02-06 14:32:08 +0900

    test: add CleanUpUtility/SetupLogTest

---
+++ Test/Case/Utility/CleanUpUtility/SetupLogTest.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * CleanUpUtility::setupLog()のテスト
+ *
+ * @author Mitsuru Mutaguchi <mutaguchi@opensource-workshop.jp>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('CleanUpCakeTestCase', 'CleanUp.TestSuite');
+App::uses('CleanUpUtility', 'CleanUp.Utility');
+
+/**
+ * CleanUpUtility::setupLog()のテスト
+ *
+ * @author Mitsuru Mutaguchi <mutaguchi@opensource-workshop.jp>
+ * @package NetCommons\CleanUp\Test\Case\Utility\CleanUpUtility
+ */
+class CleanUpUtilityCleanUpUtilitySetupLogTest extends CleanUpCakeTestCase {
+
+/**
+ * Plugin name
+ *
+ * @var string
+ */
+	public $plugin = 'clean_up';
+
+/**
+ * setupLog()のテスト
+ *
+ * @return void
+ */
+	public function testSetupLog() {
+		//テスト実施
+		// CakeLog::config()を実行してるだけなので、ロジックが通る事を確認
+		CleanUpUtility::setupLog();
+
+		//チェック
+		$this->assertTrue(true);
+	}
+
+}
