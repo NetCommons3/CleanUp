9372cc6f92a22bad237e41f53d24aa19df96de11
Mitsuru Mutaguchi <akagane99@gmail.com>
2019-02-19 17:52:29 +0900

    test: add Model/CleanUp/PrivateIsOverDelayDateTest

--- Test/Case/Model/CleanUp/PrivateIsOverDelayDateTest.php
+++ Test/Case/Model/CleanUp/PrivateIsOverDelayDateTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * CleanUp::__deleteUploadFiles()のテスト
+ * CleanUp::__isOverDelayDate()のテスト
  *
  * @author Mitsuru Mutaguchi <mutaguchi@opensource-workshop.jp>
  * @link http://www.netcommons.org NetCommons Project
@@ -12,12 +12,12 @@ App::uses('CleanUpModelTestCase', 'CleanUp.TestSuite');
 App::uses('CleanUpTestUtil', 'CleanUp.Test/Case');
 
 /**
- * CleanUp::__deleteUploadFiles()のテスト
+ * CleanUp::__isOverDelayDate()のテスト
  *
  * @author Mitsuru Mutaguchi <mutaguchi@opensource-workshop.jp>
  * @package NetCommons\CleanUp\Test\Case\Model\CleanUp
  */
-class CleanUpPrivateDeleteUploadFilesTest extends CleanUpModelTestCase {
+class CleanUpPrivateIsOverDelayDateTest extends CleanUpModelTestCase {
 
 /**
  * Fixtures
@@ -26,7 +26,7 @@ class CleanUpPrivateDeleteUploadFilesTest extends CleanUpModelTestCase {
  */
 	public $fixtures = array(
 		'plugin.clean_up.clean_up',
-		'plugin.clean_up.announcement_for_clean_up',
+		//'plugin.clean_up.announcement_for_clean_up',
 		'plugin.clean_up.block_for_clean_up',
 		'plugin.clean_up.plugin_for_clean_up',
 		'plugin.clean_up.upload_file_for_clean_up',
@@ -51,7 +51,7 @@ class CleanUpPrivateDeleteUploadFilesTest extends CleanUpModelTestCase {
  *
  * @var string
  */
-	protected $_methodName = '__deleteUploadFiles';
+	protected $_methodName = '__isOverDelayDate';
 
 /**
  * setUp method
@@ -63,13 +63,13 @@ class CleanUpPrivateDeleteUploadFilesTest extends CleanUpModelTestCase {
 	}
 
 /**
- * __deleteUploadFiles()のテスト. チェック対象unknow
+ * __isOverDelayDate()のテスト. チェック対象unknow
  *
  * @return void
  * @throws ReflectionException
- * @see CleanUp::__deleteUploadFiles()
+ * @see CleanUp::__isOverDelayDate()
  */
-	public function testDeleteUploadFilesUnknow() {
+	public function testIsOverDelayDate() {
 		$model = $this->_modelName;
 		$methodName = $this->_methodName;
 
@@ -90,29 +90,27 @@ class CleanUpPrivateDeleteUploadFilesTest extends CleanUpModelTestCase {
 		$params = $this->$model->getUploadFileParams($cleanUp);
 		$uploadFiles = $this->$model->UploadFile->find('all', $params);
 
-		// 削除対象件数 初期値
-		$targetCount = 0;
-
 		//
 		//テスト実施
 		//
 		$result = $this->_testReflectionMethod(
-			$this->$model, $methodName, array($uploadFiles, $cleanUp, $targetCount)
+			$this->$model, $methodName, array($uploadFiles[0])
 		);
 
 		//チェック
 		//var_export($result);
-		$this->assertEquals(2, $result, '2件ファイル削除する想定');
+		$this->assertFalse($result,
+			'アップロードファイルの更新日が削除遅延日より小さいのでfalseの想定');
 	}
 
 /**
- * __deleteUploadFiles()の削除遅延日以上テスト. チェック対象unknow
+ * __isOverDelayDate()の削除遅延日以上テスト. チェック対象unknow
  *
  * @return void
  * @throws ReflectionException
- * @see CleanUp::__deleteUploadFiles()
+ * @see CleanUp::__isOverDelayDate()
  */
-	public function testDeleteUploadFilesDelayDay() {
+	public function testIsOverDelayDateOver() {
 		$model = $this->_modelName;
 		$methodName = $this->_methodName;
 
@@ -134,9 +132,6 @@ class CleanUpPrivateDeleteUploadFilesTest extends CleanUpModelTestCase {
 		$uploadFiles = $this->$model->UploadFile->find('all', $params);
 		//var_dump($uploadFiles);
 
-		// 削除対象件数 初期値
-		$targetCount = 0;
-
 		// 削除遅延日 x日前
 		// ありえないほど前の日(20年～30年前)を指定して削除対象外にする
 		/* @see Cleanup::$deleteDelayDay */
@@ -146,63 +141,13 @@ class CleanUpPrivateDeleteUploadFilesTest extends CleanUpModelTestCase {
 		//テスト実施
 		//
 		$result = $this->_testReflectionMethod(
-			$this->$model, $methodName, array($uploadFiles, $cleanUp, $targetCount)
+			$this->$model, $methodName, array($uploadFiles[0])
 		);
 
 		//チェック
 		//var_export($result);
-		$this->assertEquals(0, $result, 'ファイル削除なしで0件の想定');
-	}
-
-/**
- * __deleteUploadFiles()のテスト. チェック対象announcement<br />
- * __isUseUploadFile()=trueでcontinue通しテスト
- *
- * @return void
- * @throws ReflectionException
- * @see CleanUp::__deleteUploadFiles()
- */
-	public function testDeleteUploadFilesAnnouncement() {
-		$model = $this->_modelName;
-		$methodName = $this->_methodName;
-
-		//テストデータ
-
-		//アップロードファイルで、削除対象のファイルを用意
-		CleanUpTestUtil::makeTestUploadFiles();
-
-		$data['CleanUp']['plugin_key'] = [
-			'announcements'
-		];
-
-		// チェック対象プラグイン
-		/* @see Cleanup::getCleanUpsAndPlugin() */
-		$cleanUps = $this->$model->getCleanUpsAndPlugin($data);
-		//var_dump($cleanUps);
-
-		// UploadFileインスタンスの準備
-		$this->$model->UploadFile = ClassRegistry::init('Files.UploadFile', true);
-
-		//アップロードデータ
-		/* @see Cleanup::getUploadFileParams() */
-		$params = $this->$model->getUploadFileParams($cleanUps[0]);
-		//var_dump($params);
-		$uploadFiles = $this->$model->UploadFile->find('all', $params);
-
-		// 削除対象件数 初期値
-		$targetCount = 0;
-
-		//
-		//テスト実施
-		//
-		$result = $this->_testReflectionMethod(
-			$this->$model, $methodName, array($uploadFiles, $cleanUps[0], $targetCount)
-		);
-
-		//チェック
-		//var_export($result);
-		$this->assertEquals(0, $result,
-			'アップロードファイル使ってるため、ファイル削除なしで0件の想定');
+		$this->assertTrue($result,
+			'アップロードファイルの更新日が削除遅延日以上なのでtrueの想定');
 	}
 
 }
